5877aa9de8edf67744126ee6e981d7593978a4db
hirokundayon <hirokundayon@users.noreply.github.com>
2017-03-29 21:47:22 +0900

    Update edobarai.ps1

--- edobarai.ps1
+++ edobarai.ps1
@@ -1,90 +1,214 @@
-<# 引数で起動するWebブラウザを指定 #>
-$browser = $Args -join " ";
+<# JSON付きで呼び出す #>
+function curl_json($url ,$method ,$data) {
+  $JSON = $data -replace "$([Environment]::NewLine) *","" ;
+  if ($PSVersionTable.PSVersion.Major > 2) {
+    $AdoDbStream = New-Object -ComObject ADODB.Stream
+ 
+    $AdoDbStream.Charset = "UTF-8"
+    $AdoDbStream.Type = 2
+    $AdoDbStream.Open()
 
-<# selenium-standalone-server 起動 #>
-<# ./selenium-standalone-server.bat #>
+    $AdoDbStream.WriteText($JSON)
 
-switch ($browser)
-{
+    $pos = $AdoDbStream.Position
+    $AdoDbStream.Position = 0
+    $AdoDbStream.Type = 1
+    $JSON=$AdoDbStream.Read($pos)
+
+    $returnValue=Invoke-RestMethod -Uri $url -Method $method -Body $JSON
+    $AdoDbStream.Close()
+  } else {
+    <# PowerShell V2用 #>
+    $Request = [System.Net.WebRequest]::Create($url);
+    $Request.Method = $method;
+    $Request.ContentType = "application/json";
+    $Request.Accept = "application/json";
+
+    $utf8Bytes = [System.Text.Encoding]::UTF8.GetBytes($JSON);
+    $request.ContentLength = $utf8Bytes.Length;
+    $postStream = $request.GetRequestStream();
+    $postStream.Write($utf8Bytes, 0, $utf8Bytes.Length);
+    $postStream.Dispose();
+
+    $response = $request.GetResponse();
+
+    $reader = [IO.StreamReader] $response.GetResponseStream();
+    $output = $reader.ReadToEnd();
+
+    $reader.Close();
+    $response.Close();
+
+    add-type -assembly system.web.extensions
+    $ps_js=new-object system.web.script.serialization.javascriptSerializer
+    $returnValue=,$ps_js.DeserializeObject($output)
+  }
+
+  Write-Output $returnValue
+}
+
+<# JSONなしで呼び出す #>
+function curl_nothing($url ,$method) {
+  if ($PSVersionTable.PSVersion.Major > 2) {
+    $returnValue=Invoke-RestMethod -Uri $url -Method $method
+  } else {
+    <# PowerShell V2用 #>
+    $Request = [System.Net.WebRequest]::Create($url);
+    $Request.Method = $method;
+    $Request.ContentType = "application/json";
+    $Request.Accept = "application/json";
+
+    $response = $request.GetResponse();
+
+    $reader = [IO.StreamReader] $response.GetResponseStream();
+    $output = $reader.ReadToEnd();
+
+    $reader.Close();
+    $response.Close();
+
+    add-type -assembly system.web.extensions
+    $ps_js=new-object system.web.script.serialization.javascriptSerializer
+    $returnValue=,$ps_js.DeserializeObject($output)
+  }
+
+  Write-Output $returnValue
+}
+
+<# セッションを起動する #>
+function newSession($browser) {
+  $url="http://localhost:4444/wd/hub/session"
+  $method="POST"
+  switch ($browser)
+  {
     <# Mozilla Firefox #>
     "Firefox" {
-            $data = '{"desiredCapabilities":{"browserName":"firefox"},"requiredCapabilities":{}}'
+            $JSON='{"desiredCapabilities":{"browserName":"firefox"},"requiredCapabilities":{}}'
         }
     <# Google Chrome #>
     "chrome" {
-            $data = '{"desiredCapabilities":{"browserName":"chrome"},"requiredCapabilities":{}}'
+            $JSON='{"desiredCapabilities":{"browserName":"chrome"},"requiredCapabilities":{}}'
         }
-    <# Internet Explorer #>
+    <# Internet Explorer Win32版 #>
     "ie" {
-            $data = '{"desiredCapabilities":{"browserName":"internet explorer"},"requiredCapabilities":{}}'
+            $JSON = '{"desiredCapabilities":{"browserName":"internet explorer"},"requiredCapabilities":{}}'
         }
-    <# Edge #>
     "Edge" {
-            $data = '{"desiredCapabilities":{"browserName":"MicrosoftEdge"},"requiredCapabilities":{}}'
-        }
-    <# AndroidのGoofle Chrome #>
-    "Android" {
-            $data = '{"desiredCapabilities":{"chromeOptions": {"androidPackage":"com.android.chrome"},"browserName":"chrome"},"requiredCapabilities":{}}'
-        }
+            $JSON = '{"desiredCapabilities":{"browserName":"MicrosoftEdge"},"requiredCapabilities":{}}'
+       }
     <# それ以外の場合は Google Chrome #>
     default {
-            $browser = "chrome";
-            $data = '{"desiredCapabilities":{"browserName":"chrome"},"requiredCapabilities":{}}'
+            $JSON='{"desiredCapabilities":{"browserName":"chrome"},"requiredCapabilities":{}}'
         }
+  }
+
+  $response=curl_json $url $method $JSON
+  Write-Output $response.sessionId
 }
 
-$url = "http://localhost:4444/wd/hub/session"
-$method = "POST"
 
-$response = Invoke-RestMethod -Uri $url -Body $data -Method $method
-$sessionId = $response.sessionId
+<# セッションを閉じる #>
+function closeSession($sessionId) {
+  $url="http://localhost:4444/wd/hub/session/$sessionId"
+  $method="DELETE"
+  $response=curl_nothing $url $method
+}
 
-<# Webブラウザのウィンドウを最大化 #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId/window/maximize"
-$method = "POST"
-$response = Invoke-RestMethod -Uri $url -Method $method
 
-<# Googleのページへ移動 #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId/url"
-$method = "POST"
-$data = '{"url":"https://www.google.co.jp/"}'
-$response = Invoke-RestMethod -Uri $url -Body $data -Method $method
+<# Windowを最大化 #>
+function maximizeWindow($sessionId) {
+  $url="http://localhost:4444/wd/hub/session/$sessionId/window/maximize"
+  $method = "POST"
+  $response=curl_nothing $url $method
+}
 
-<# 表示待ち #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId/title"
-$method = "GET"
-do
-{
-  Start-Sleep -s 1
-  $response = Invoke-RestMethod -Uri $url -Method $method
-  $title = $response.value
-} until($title.Contains("Google"))
 
-<# 検索 #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId/element"
-$method = "POST"
-$data = '{"using":"name","value":"q"}'
-$response = Invoke-RestMethod -Uri $url -Body $data -Method $method
-$elementId = $response.value.ELEMENT
+<# URLへ移動 #>
+function goURL($sessionId, $URL) {
+  $JSON='{"url":"' + $URL + '"}'
+  $url="http://localhost:4444/wd/hub/session/$sessionId/url"
+  $method="POST"
+  $response=curl_json $url $method $JSON
+}
 
-$url = "http://localhost:4444/wd/hub/session/$sessionId/element/$elementId/value"
-$method = "POST"
-$data = '{"value":["OSC Fukuoka\n"]}'
-$response = Invoke-RestMethod -Uri $url -Body $data -Method $method
-
-<# 検索結果が表示されるまで待つ #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId/title"
-$method = "GET"
-do
-{
-  Start-Sleep -s 1
-  $response = Invoke-RestMethod -Uri $url -Method $method
+<# タイトル取得 #>
+function getTitle($sessionId) {
+  $url="http://localhost:4444/wd/hub/session/$sessionId/title"
+  $method="GET"
+  $response=curl_nothing $url $method
   $title = $response.value;
-} until($title.Contains("OSC Fukuoka"))
+  Write-Output $title
+}
 
-Start-Sleep -s 10
+<# タイトル表示待ち #>
+<# タイトル1つ #>
+function waitForTitle($sessionId, $title1) {
+  $url="http://localhost:4444/wd/hub/session/$sessionId/title"
+  $method="GET"
+  do
+  {
+    Start-Sleep -s 1
+    $response=curl_nothing $url $method
+    $title = $response.value;
+  } until($title.Contains($title1))
+  Write-Output $title
+}
 
-<# セッション終了 #>
-$url = "http://localhost:4444/wd/hub/session/$sessionId"
-$method = "DELETE"
-$response = Invoke-RestMethod -Uri $url -Method $method
+<# タイトル2つ #>
+function waitForTwoTitles($sessionId, $title1, $title2) {
+  $url="http://localhost:4444/wd/hub/session/$sessionId/title"
+  $method="GET"
+  do
+  {
+    Start-Sleep -s 1
+    $response=curl_nothing $url $method
+    $title = $response.value;
+  } until($title.Contains($title1) -or $title.Contains($title2))
+  Write-Output $title
+}
+
+<# 部品取得 #>
+function getElement($sessionId, $selector, $locator) {
+  $JSON='{"using":"' + $selector + '","value":"' + $locator + '"}'
+  $url="http://localhost:4444/wd/hub/session/$sessionId/element"
+  $method="POST"
+  do
+  {
+     $response=curl_json $url $method $JSON
+  } until($response.status -eq 0)
+  Write-Output $response.value.ELEMENT
+}
+
+<# By name #>
+function getElementByName($sessionId, $name) {
+  $response=getElement $sessionId "name" $name
+  Write-Output $response
+}
+
+<# By partial link text #>
+function getElementByPartialLinkText($sessionId, $PartialLinkText) {
+  $response=getElement $sessionId "partial link text" $PartialLinkText
+  Write-Output $response
+}
+
+<# By css selector #>
+function getElementByCSSselector($sessionId, $CSSselector) {
+  $response=getElement $sessionId "css selector" $CSSselector
+  Write-Output $response
+}
+
+<# Click Element #>
+function clickElement($sessionId, $element) {
+  $method="POST"
+  $url="http://localhost:4444/wd/hub/session/$sessionId/element/$element/click"
+
+  $response=curl_nothing $url $method
+}
+
+<# Send Keys to Element #>
+function sendKeysToElement($sessionId, $element, $text) {
+  $JSON='{"value":["' + $text + '"]}'
+  $method="POST"
+  $url="http://localhost:4444/wd/hub/session/$sessionId/element/$element/value"
+
+  $response=curl_json $url $method $JSON
+}
+
