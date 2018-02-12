4c3c191e9129344436c38dd9066366656cee92d0
hirokundayon <hirokundayon@users.noreply.github.com>
2018-02-12 20:19:57 +0900

    Google Chrome のみの制御に変更。

--- edobarai.ps1
+++ edobarai.ps1
@@ -7,49 +7,47 @@ $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $browser = $Args -join " ";
 
 <# ブラウザ起動 #>
-$sessionId=newSession $browser
-
-<# Windowを最大化 #>
-<# maximizeWindow $sessionId #>
+$sessionURL=newSession
 
 <# Googleのページへ移動 #>
-goURL $sessionId "http://www.google.co.jp/"
+goURL $sessionURL "http://www.google.co.jp/"
 
 <# 表示待ち #>
-waitForTitle $sessionId "Google"
+waitForTitle $sessionURL "Google"
 
 <# 「OSC Fukuoka 太宰府」を検索 #>
-$element=getElementByName $sessionId "q"
+$element=getElementByName $sessionURL "q"
 
-sendKeysToElement $sessionId $element "OSC Fukuoka 太宰府\n"
+sendKeysToElement $sessionURL $element "OSC Fukuoka 太宰府\n"
 
 <# 検索結果が表示されるまで待つ #>
-waitForTitle $sessionId "OSC Fukuoka 太宰府 - Google 検索"
+waitForTitle $sessionURL "OSC Fukuoka 太宰府 - Google 検索"
 
 <# OSC2011 Fukuoka のレポートを表示させる #>
-$element=getElementByPartialLinkText $sessionId "OSC2011 Fukuoka 太宰府にて開催！！"
+$element=getElementByPartialLinkText $sessionURL "OSC2011 Fukuoka 太宰府にて開催！！"
 
-clickElement $sessionId $element
+clickElement $sessionURL $element
 
 <# レポートが表示されるまで待つ #>
-waitForTitle $sessionId "OSC2011 Fukuoka 太宰府にて開催！！"
+waitForTitle $sessionURL "OSC2011 Fukuoka 太宰府にて開催！！"
 
 <# LTの写真を表示 #>
 $CSSselector = "img[title='仏様に見守られながらのLT']";
-$element=getElementByCSSselector $sessionId $CSSselector
+$element=getElementByCSSselector $sessionURL $CSSselector
 
-clickElement $sessionId $element
+clickElement $sessionURL $element
 
 <# 写真が表示されるまで待つ #>
-waitForTitle $sessionId "仏様に見守られながらのLT"
+waitForTitle $sessionURL "仏様に見守られながらのLT"
 
 <# 写真をクリック #>
 $CSSselector = "a[href='https://www.ospn.jp/press/wp-content/uploads/2011/12/LT.jpg']"
-$element=getElementByCSSselector $sessionId $CSSselector
+$element=getElementByCSSselector $sessionURL $CSSselector
 
-clickElement $sessionId $element
+clickElement $sessionURL $element
 
 <# 10秒待つ #>
 Start-Sleep -s 10
 <# 終了処理 #>
-closeSession $sessionId
+closeSession $sessionURL
+
